a3bf2a207a2d520b415ca2ba6c1dff3856fe1f0f
Rachel McGreevy <rachelmcgreevy@gmail.com>
2017-02-21 21:36:43 +0000

    Change tube map to pdf file
    Change tube map to pdf file and change to uiwebview to view the file.
    Much better resolution map.

--- London Underground Tube Status/SecondViewController.h
+++ London Underground Tube Status/SecondViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SecondViewController : UIViewController <UIScrollViewDelegate>
+@interface SecondViewController : UIViewController <UIWebViewDelegate, UIScrollViewDelegate>
 
 
 @end
--- London Underground Tube Status/SecondViewController.m
+++ London Underground Tube Status/SecondViewController.m
@@ -21,31 +21,19 @@
     // Do any additional setup after loading the view, typically from a nib.
     CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
     CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
-    UIScrollView *vc = [[UIScrollView alloc] initWithFrame:CGRectMake(0,0,screenWidth,screenHeight)];
-    //vc.backgroundColor = [UIColor whiteColor];
-    vc.minimumZoomScale=0.5;
-    vc.zoomScale=1.5;
-    vc.maximumZoomScale=3.0;
-    vc.contentSize=CGSizeMake(1700, 1140);
-    _backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 25, 1700, 1140)];
-    _backgroundImage.image = [UIImage imageNamed:@"Graphics/tubemap.png"];
-    _backgroundImage.contentMode = UIViewContentModeScaleAspectFit;
-    [vc addSubview:_backgroundImage];
-    vc.delegate=self;
-    [vc setBackgroundColor:[UIColor whiteColor]];
+    UIWebView *vc = [[UIWebView alloc] initWithFrame:CGRectMake(0, 30, screenWidth, screenHeight)];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Graphics/tubemap2" ofType:@"pdf"];
+    NSURL *detailURL = [NSURL fileURLWithPath:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:detailURL];
+    [vc loadRequest:request];
+    vc.scalesPageToFit = YES;
+    vc.backgroundColor = [UIColor whiteColor];
     self.view = vc;
 }
-
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-{
-    return self.backgroundImage;
-}
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
